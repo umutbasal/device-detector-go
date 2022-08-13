b73f02045727aaaee4fb3c5d2e3580d4d6963e4d
umutbasal <umut.bsl@yandex.com>
2022-08-13 04:44:58 +0300

    init poc

---
+++ build/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	_ "embed"
+	"log"
+
+	esbuild "github.com/evanw/esbuild/pkg/api"
+)
+
+func main() {
+	// run npm install in the project directory
+	// exec := exec.Command("npm", "install", "--legacy-peer-deps")
+	// exec.Dir = "./device-detector-js"
+	// exec.Stdout = os.Stdout
+	// exec.Stderr = os.Stderr
+	// if err := exec.Run(); err != nil {
+	// 	log.Fatal(err)
+	// }
+	// run esbuild in the project directory
+	bundleContent := esbuild.Build(esbuild.BuildOptions{
+		EntryPoints: []string{"device-detector-js/src/index.ts"},
+		Outfile:     "dist/device-detector.js",
+		Bundle:      true,
+		Write:       true,
+		Target:      esbuild.ES2015,
+		Format:      esbuild.FormatIIFE,
+	})
+	if len(bundleContent.OutputFiles) == 0 {
+		log.Fatalf("no output files%s", bundleContent.Errors)
+	}
+}
---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/umutbasal/devicedetector-go
+
+go 1.18
+
+require (
+	github.com/evanw/esbuild v0.15.1
+	rogchap.com/v8go v0.7.0
+)
+
+require golang.org/x/sys v0.0.0-20220715151400-c0bba94af5f8 // indirect
